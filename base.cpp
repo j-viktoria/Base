ca3a1d8c19551759dc6899abeaed86833b988586
j-viktoria <vikysia_96@mail.ru>
2014-03-22 16:53:58 +0300

    4

---
+++ base.cpp
@@ -0,0 +1,22 @@
+#include "base.h"
+
+int sortFamilia (struct TAbonent abn1,struct TAbonent abn2)
+{
+
+}
+int sortData (struct TAbonent abn1,struct TAbonent abn2)
+{
+
+}
+int sortStag (struct TAbonent abn1,struct TAbonent abn2)
+{
+
+}
+int sortPooshrenia (struct TAbonent abn1,struct TAbonent abn2)
+{
+
+}
+int sortVziskania (struct TAbonent abn1,struct TAbonent abn2)
+{
+
+}
